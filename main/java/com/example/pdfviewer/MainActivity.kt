0ba295358c95eff0b33d023a14308d5fe2257cf1
MateuszLan <92538483+MateuszLan@users.noreply.github.com>
2022-10-27 11:14:08 +0200

    pdf files

---
+++ main/java/com/example/pdfviewer/MainActivity.kt
@@ -0,0 +1,17 @@
+
+package com.example.pdfviewer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pdfview.PDFView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<PDFView>(R.id.activityMainPdfView).fromAsset("dokument.pdf").show(
+
+        )
+    }
+}
